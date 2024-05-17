e06fcdaccf9ee63eb2d37e3fb01a4d0831c36552
CXC2001 <xinchengchen2001@gmail.com>
2024-05-17 21:09:19 +0200

    solverb2b

--- src/BacktoBack.jl
+++ src/BacktoBack.jl
@@ -2,19 +2,28 @@ using Unfold
 using UnfoldMakie, CairoMakie
 using UnfoldSim
 using UnfoldDecode
+using DataFrames
 dat, evts = UnfoldSim.predef_eeg(; noiselevel = 10, return_epoched = true)
+# 如果名为condition的列的取值为"car"，则重新赋值为"dog"；
+evts = rename(evts,:condition => :animal)
+evts.animal .= ifelse.(evts.animal .== "car", "dog", evts.animal)
+# 如果是"face"，则重新赋值为"cat"
+evts.animal .= ifelse.(evts.animal .== "face", "cat", evts.animal)
+evts.continuous_random .= rand(size(evts,1)) #生成了2000个0-1的随机数
+evts.vegetable .= ["tomato","carrot"][1 .+ (evts.continuous .+ 10 .* rand(size(evts,1)) .> 7.5)] # make random samples with a correlation of e.g. 0.5 to evts.continuous
 
-evts.continuous_random .= rand(size(evts,1))
-evts.categorical_correlated .= evts.continuous .+ 10 .*rand(size(evts,1)).>7.5 # make random samples with a correlation of e.g. 0.5 to evts.continuous
-f = @formula 0 ~ 1  + continuous 
-
-f = @formula 0 ~ 1  + categorical_correlated 
-
-f = @formula 0 ~ 1  + continuous + categorical_correlated
-
-
-f = @formula 0 ~ 1 + condition + continuous + continuous_random
-designDict = [Any => (f, range(0, 0.44, step = 1/100))]
+results_all = DataFrame()
+for ix = 1:4
+    if ix == 1
+        f = @formula 0 ~ 1  + animal + continuous 
+    elseif ix==2
+        f = @formula 0 ~ 1  + animal + vegetable 
+    elseif ix==3
+        f = @formula 0 ~ 1  + animal + continuous + vegetable
+    elseif ix ==4
+        f = @formula 0 ~ 1 + animal + continuous + continuous_random
+    end
+designDict = [Any => (f, range(0, 0.44, step = 1/100))] # designDict是一个字典，key是Any，value是一个元组，元组的第一个元素是f，第二个元素是range(0, 0.44, step = 1/100)
 
 
 b2b_solver = (x, y) -> UnfoldDecode.solver_b2b(x, y; cross_val_reps = 5)
@@ -24,5 +33,9 @@ results = coeftable(m)
 results.estimate = abs.(results.estimate)
 results = results[results.coefname .!="(Intercept)",:]
 #---
+results.formula .= string(f)
+results_all = vcat(results_all,results)
 
-plot_erp(results)
+
+end
+plot_erp(results_all;mapping=(;row=:formula))
